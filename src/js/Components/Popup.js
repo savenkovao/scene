1a19fdd466972a4d3e19e4f6c1e5f1dacf388f62
savienkov <as@attractgroup.com>
2019-07-29 22:04:39 +0300

    fix

--- src/js/Components/Popup.js
+++ src/js/Components/Popup.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
-
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 require('../../../node_modules/remodal/src/remodal');
 
 export class Popup {
 
   constructor(selector, options) {
     this.selector = selector;
+    this.$target = $(this.selector);
 
     this.defaulOptions = {};
 
@@ -16,26 +17,12 @@ export class Popup {
   _initPopup() {
     console.log('Modal inited');
     $(document).on('opened', this.selector, function () {
-      console.log('Modal is opened');
-      stopBodyScrolling(true);
+      disableBodyScroll(this.$target[0]);
     });
 
     $(document).on('closed', '.remodal', function (e) {
-      console.log('Modal is closed' + (e.reason ? ', reason: ' + e.reason : ''));
-      stopBodyScrolling(false);
+      enableBodyScroll(this.$target[0]);
     });
-
-    let freezeVp = function (e) {
-      e.preventDefault();
-    };
-
-    function stopBodyScrolling(bool) {
-      if (bool) {
-        document.body.addEventListener("touchmove", freezeVp, false);
-      } else {
-        document.body.removeEventListener("touchmove", freezeVp, false);
-      }
-    }
 
     return $(this.selector).remodal(this.options);
   }
