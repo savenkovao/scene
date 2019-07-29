02198d6865889612c7a138de454d3d2040c15c9f
savienkov <as@attractgroup.com>
2019-07-29 21:10:05 +0300

    fix

--- src/js/Components/Popup.js
+++ src/js/Components/Popup.js
@@ -1,19 +1,42 @@
 import $ from 'jquery';
+
 require('../../../node_modules/remodal/src/remodal');
 
 export class Popup {
 
-    constructor(selector, options) {
-        this.selector = selector;
+  constructor(selector, options) {
+    this.selector = selector;
 
-        this.defaulOptions = {};
+    this.defaulOptions = {};
 
-        this.options = $.extend(this.defaulOptions, options);
-        return this._initPopup();
+    this.options = $.extend(this.defaulOptions, options);
+    return this._initPopup();
+  }
+
+  _initPopup() {
+    $(document).on('opened', this.selector, function () {
+      console.log('Modal is opened');
+      stopBodyScrolling(true);
+    });
+
+    $(document).on('closed', '.remodal', function (e) {
+      console.log('Modal is closed' + (e.reason ? ', reason: ' + e.reason : ''));
+      stopBodyScrolling(true);
+    });
+
+    let freezeVp = function (e) {
+      e.preventDefault();
+    };
+
+    function stopBodyScrolling(bool) {
+      if (bool) {
+        document.body.addEventListener("touchmove", freezeVp, false);
+      } else {
+        document.body.removeEventListener("touchmove", freezeVp, false);
+      }
     }
 
-    _initPopup() {
-        return $(this.selector).remodal(this.options);
-    }
+    return $(this.selector).remodal(this.options);
+  }
 
 }
